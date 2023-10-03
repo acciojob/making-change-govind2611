4fa9828b7216f4e3dc7af9cff76761b9092107a6
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-10-03 08:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const makeChange = (c) => {
-  // your name here
+  if (c <= 0 || c >= 100) {
+    return "Invalid input. The amount should be between 1 and 99 cents.";
+  }
+
+  const quarters = Math.floor(c / 25);
+  c %= 25;
+
+  const dimes = Math.floor(c / 10);
+  c %= 10;
+
+  const nickels = Math.floor(c / 5);
+  c %= 5;
+
+  const pennies = c;
+
+  return { "q": quarters, "d": dimes, "n": nickels, "p": pennies };
 };
 
-// Do not the change the code below
+// Example usage:
 const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+const result = makeChange(c);
+console.log(result);
